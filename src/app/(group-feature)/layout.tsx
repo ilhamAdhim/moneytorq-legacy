e6a2f06fbebdb80e7690a2f8a4653acd10a96ac7
ilhamadhim <ilhamm179@gmail.com>
2024-04-20 14:42:28 +0700

    chore: added dark mode + theme switcher

--- src/app/(group-feature)/layout.tsx
+++ src/app/(group-feature)/layout.tsx
@@ -1,5 +1,6 @@
 import { MainNav } from "@/components/composites/main-nav";
 import TeamSwitcher from "@/components/composites/team-switcher";
+import ThemeSwitcher from "@/components/composites/theme-switcher";
 import { Search } from "lucide-react";
 
 interface ILayoutGroupFeature {
@@ -8,25 +9,26 @@ interface ILayoutGroupFeature {
 
 function LayoutGroupFeature({ children }: ILayoutGroupFeature) {
     return (
-    <>
-        <div className="hidden flex-col md:flex">
-            <div className="border-b">
-                <div className="flex h-16 items-center px-4">
-                    <TeamSwitcher />
-                    <MainNav className="mx-6" />
-                    <div className="ml-auto flex items-center space-x-4">
-                        <Search />
-                        {/* <UserNav /> */}
+        <>
+            <div className="hidden flex-col md:flex">
+                <div className="border-b">
+                    <div className="flex h-16 items-center px-4">
+                        <TeamSwitcher />
+                        <MainNav className="mx-6" />
+                        <div className="ml-auto flex items-center space-x-4">
+                            <ThemeSwitcher />
+                            {/* <Search /> */}
+                            {/* <UserNav /> */}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className="flex-1 space-y-4 p-8 pt-6">
-            {children}
-        </div>
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                {children}
+            </div>
 
-    </>
+        </>
     );
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import { ITransaction } from "@/types/transactionTypes";
 import { atomWithStorage } from "jotai/utils";
 
 // Global Atoms 
-export const colorMode = atomWithStorage<"light" | "dark">("__colorMode", "light")
+export const colorMode = atomWithStorage<"light" | "dark" | "system">("__colorMode", "system")
 
 // Transaction Records -> Holding all the inputted data transaction from user
 export const transactionRecords = atomWithStorage<ITransaction[]>("__transactionRecords", [])
