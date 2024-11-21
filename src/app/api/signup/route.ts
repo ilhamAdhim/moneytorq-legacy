3072dd19b90375b9db4bea131d9709115e2d596b
ilhamadhim <ilhamm179@gmail.com>
2024-11-21 17:55:10 +0700

    feat: update sender moneytorq send OTP

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -5,29 +5,30 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-	// rate limit
+  // rate limit
 
-	const data = await request.json();
-	const supabase = supabaseAdmin();
+  const data = await request.json();
+  const supabase = supabaseAdmin();
 
-	const res = await supabase.auth.admin.generateLink({
-		type: "signup",
-		email: data.email,
-		password: data.password,
-	});
+  const res = await supabase.auth.admin.generateLink({
+    type: "signup",
+    email: data.email,
+    password: data.password,
+  });
 
-	if (res.data.properties?.email_otp) {
-		// resend email
-		const resendRes = await resend.emails.send({
-			from: `Acme <onboarding@${process.env.RESEND_DOMAIN}>`,
-			to: [data.email],
-			subject: "Verify Email",
-			react: SupaAuthVerifyEmail({
-				verificationCode: res.data.properties?.email_otp,
-			}),
-		});
-		return Response.json(resendRes);
-	} else {
-		return Response.json({ data: null, error: res.error });
-	}
+  if (res.data.properties?.email_otp) {
+    // resend email
+    const resendRes = await resend.emails.send({
+      from: `System <no-reply@${process.env.RESEND_DOMAIN}>`,
+      to: [data.email],
+      subject: "Verify Email",
+      react: SupaAuthVerifyEmail({
+        verificationCode: res.data.properties?.email_otp,
+      }),
+    });
+
+    return Response.json(resendRes);
+  } else {
+    return Response.json({ data: null, error: res.error });
+  }
 }
